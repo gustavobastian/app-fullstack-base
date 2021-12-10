93b20a1ea75d13ebe4ede2a0c7499d8e1e9f45a3
Gustavo Bastian <bastian.gustavo@gmail.com>
2021-12-10 15:25:53 -0300

    playing with db init

---
+++ src/tools/wait-for-db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for-db.sh
+
+set -e
+  
+
+shift
+mysql_user=root
+mysql_pass=userpass
+target_db=smart_home
+
+until  { "`mysql -u${mysql_user} -p${mysql_pass} -e 'show databases;' | grep ${target_db}`" == "${target_db}" }; do
+  >&2 echo "db is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "db is up - executing command"
+exec "$@"
